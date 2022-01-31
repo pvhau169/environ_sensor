2b455073a8fd74ec5ad5d597ccc9f756acb43fee
Hau Phan <41348200+pvhau169@users.noreply.github.com>
2022-01-31 18:51:42 -0500

    add real time count

--- print_out_light_sensor.py
+++ print_out_light_sensor.py
@@ -19,28 +19,51 @@ from time import sleep
 import argparse
 import itertools
 import os
+import numpy as np
+import time
+
 DEFAULT_CONFIG_LOCATION = os.path.join(os.path.dirname(__file__), 'cloud_config.ini')
+
+
 def update_display(display, msg):
     with canvas(display) as draw:
         draw.text((0, 0), msg, fill='white')
+
+
 def _none_to_nan(val):
     return float('nan') if val is None else val
+
+
 def main():
     # Pull arguments from command line.
-    total_display = 300
+    total_display = 10
     display_duration = 0.2
     # Create instances of EnviroKit and Cloud IoT.
-    enviro = EnviroBoard()
-    
+    enviro_sensor = EnviroBoard()
+    start = time.time()
+
     # Indefinitely update display and upload to cloud.
-    sensors = {}
+    sensors = {'ambient_light': []}
     read_period = int(total_display / display_duration)
-    for read_count in itertools.count():
-        #dis play light sensor
-        sensors['ambient_light'] = enviro.ambient_light
-        msg = 'Light: %.2f lux\n' % _none_to_nan(sensors['ambient_light'])
-        update_display(enviro.display, msg)
-        sleep(display_duration)
-        print(enviro.ambient_light)
+    light_value = 0
+    for read_count in range(read_period):
+        # dis play light sensor
+        light_value = enviro_sensor.ambient_light
+        sensors['ambient_light'].append(light_value)
+        msg = 'Light: %.2f lux\n' % _none_to_nan(light_value)
+        # update_display(enviro_sensor.display, msg)
+
+        # print log
+        real_time = time.time() - start
+        print(light_value, read_count, real_time)
+        # sleep(display_duration)
+
+    # write value to file
+    value_to_file = np.array(sensors['ambient_light'])
+    write_file = open("sensor_value.txt", "w")
+    np.savetxt(write_file, value_to_file)
+    write_file.close()
+
+
 if __name__ == '__main__':
     main()
